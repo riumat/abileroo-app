de43d25f64c088ea6efb22ddb69dfc60e33f3936
riumat <matteoariu42@gmail.com>
2024-05-30 17:56:55 +0200

    fix home page carousel images dimensions

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -14,7 +14,7 @@ const Carousel = () => {
     <div className="overflow-hidden relative  h-40 flex items-center rounded-b-lg rounded-tr-lg shadow w-full bg-white dark:bg-slate-950 p-2">
       <div className="flex absolute l-0 justify-center items-center gap-4 w-[200%] h-full carousel">
         {shopList?.map((shop,i) => (
-          <div key={`carousel-img-${i}`} className="flex justify-center items-center">
+          <div key={`carousel-img-${i}`} className="w-32 h-32 flex justify-center items-center">
             <img src={shop?.image} className="object-contain rounded-lg" alt="" />
           </div>
         ))
