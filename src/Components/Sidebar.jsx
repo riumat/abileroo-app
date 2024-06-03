8855267bdbccfdb086bf72dd5a9aaf1c2a2bd22c
riumat <matteoariu42@gmail.com>
2024-06-03 14:20:16 +0200

    removed not used options in sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { IoIosTrendingUp } from "react-icons/io";
 import { FaShop } from "react-icons/fa6";
 import { RiDiscountPercentLine } from "react-icons/ri";
 import { MdOutlineDiscount, MdLogout, MdBorderColor } from "react-icons/md";
-import { FaShoppingCart, FaHouseUser } from "react-icons/fa";
+import { FaHouseUser } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -16,95 +16,62 @@ const Sidebar = ({ isSideOpen }) => {
 
   if (!isSideOpen) return;
   return (
-    <div className={`rounded-lg p-5 absolute md:static z-50 text-[13px] bg-light  ${isCompressed ? "w-18" : "w-[200px]"}`}>
+    <div className={`rounded-lg p-5 absolute md:static z-50 text-[13px] bg-light shadow-none  flex flex-col gap-16 ${isCompressed ? "w-18" : "w-[200px]"}`}>
 
       <div className="md:flex justify-center hidden">
-        <IoMenu className="w-7 h-7 p-1 rounded-lg cursor-pointer hover:bg-emerald-800 dark:hover:bg-emerald-800 hover:text-white dark:text-slate-100" onClick={() => setIsCompressed(prev => !prev)} />
+        <IoMenu
+          className="w-7 h-7 p-1 rounded-lg cursor-pointer hover:bg-emerald-800 dark:hover:bg-emerald-800 hover:text-white dark:text-slate-100"
+          onClick={() => setIsCompressed(prev => !prev)}
+        />
       </div>
 
-      <div className="flex flex-col gap-5 border-b border-slate-400  py-5">
-        {!isCompressed && (
-          <p className="font-semibold">Shops</p>
-        )}
-        <div className="flex flex-col gap-2 text-slate-700 dark:text-slate-200 ">
-          <div className="sb-button">
-            <IoIosTrendingUp className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Trending</p>
-            )}
-          </div>
+      <div className="flex flex-col gap-12  ">
 
-          <Link to={"/find"} className="sb-button" >
-            <FaShop className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Find All</p>
-            )}
-          </Link>
 
+        <Link to={"/find"} className="sb-button" >
+          <FaShop className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Shops</p>
+          )}
+        </Link>
+
+        <div className="sb-button">
+          <MdOutlineDiscount className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Products</p>
+          )}
         </div>
+
+
+        <Link to={"/home"} className="sb-button">
+          <FaHouseUser className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Home</p>
+          )}
+        </Link>
+
+        <Link to={"/favorites"} className="sb-button">
+          <BsHouseHeart className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Favorites</p>
+          )}
+        </Link>
+
+        <Link to={"/orders"} className="sb-button">
+          <MdBorderColor className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Orders</p>
+          )}
+        </Link>
+
+        <Link to={"/"} className="sb-button">
+          <MdLogout className="w-5 h-5" />
+          {!isCompressed && (
+            <p>Log out</p>
+          )}
+        </Link>
       </div>
 
-      <div className="flex flex-col gap-5 border-b border-slate-400 py-5">
-        {!isCompressed && (
-          <p className="font-semibold">Products</p>
-        )}
-
-        <div className="flex flex-col gap-2 text-slate-700 dark:text-slate-200">
-          <div className="sb-button">
-            <RiDiscountPercentLine className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Discounts</p>
-            )}
-          </div>
-
-          <div className="sb-button">
-            <MdOutlineDiscount className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Find All</p>
-            )}
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-5  py-5">
-        {!isCompressed && (
-          <p className="font-semibold">Your Area</p>
-        )}
-
-
-
-        <div className="flex flex-col gap-2 text-slate-700 dark:text-slate-200">
-
-          <Link to={"/home"} className="sb-button">
-            <FaHouseUser className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Home</p>
-            )}
-          </Link>
-
-          <Link to={"/favorites"} className="sb-button">
-            <BsHouseHeart className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Favorites</p>
-            )}
-          </Link>
-
-          <Link to={"/orders"} className="sb-button">
-            <MdBorderColor className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Orders</p>
-            )}
-          </Link>
-
-          <Link to={"/"} className="sb-button">
-            <MdLogout className="w-5 h-5" />
-            {!isCompressed && (
-              <p>Log out</p>
-            )}
-          </Link>
-
-        </div>
-      </div>
     </div>
   )
 }
